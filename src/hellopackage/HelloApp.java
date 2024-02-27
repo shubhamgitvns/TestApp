ca3e4bd212b1d7d63b814002dfc0645aeb3d6db9
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-02-27 11:36:41 +0530

    day 2

--- src/hellopackage/HelloApp.java
+++ src/hellopackage/HelloApp.java
@@ -3,72 +3,21 @@ package hellopackage;
 import java.util.Scanner;
 
 public class HelloApp {
-
-    static void Check_Poss_Nig() {
-        int a[] = {1, 2, -3, 4, -5, 6};
-        int i, pos = 0, neg = 0;
-        for (i = 0; i < a.length; i++) {
-            if (a[i] > 0) {
-                pos++;
-            }
+    static void Sumof_Set(){
+        int arr[]={1,2,3,4,5,6,7,8,9,-3};
+        int i,sum=0;
+        for(i=0;i<arr.length;i++){
+           sum=sum+arr[i];
         }
-        System.out.println("positive ineger= " + pos);
-        System.out.println("negative ineger= " + (a.length - pos));
-
+         System.out.print(sum);
     }
 
-    static void Check_Pass() {
-        int pass = 0, fail = 0;
-        Scanner Obj = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter number:");
-            int num = Obj.nextInt();
-            if (num < 0) {
-                break;
-            }
-            if (num < 40) {
-                fail++;
-            } else {
-                pass++;
-            }
-        }
-        System.out.println("Pass=" + pass);
-        System.out.println("fail=" + fail);
-
-    }
-
-    static void Exchange() {
-        int arr[] = {1,2,3,4,5};
-        int i, n = 1;
-
-        for (i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ",");
-        }
-        for (i = 0; i < n; i++) {
-            int j, last;
-            last = arr[arr.length - 1];
-
-            for (j = arr.length - 1; j > 0; j--) {
-
-                arr[j] = arr[j - 1];
-            }
-
-            arr[0] = last;
-        }
-
-        System.out.println();
-
-        System.out.println("Array after right rotation: ");
-        for (i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-
-    }
+   
 
     public static void main(String[] args) {
+        Sumof_Set();
+                
 
-        // Check_Poss_Nig();
-        Exchange();
-
+       
     }
 }
