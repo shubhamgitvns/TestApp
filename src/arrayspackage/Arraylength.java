aa9646d2949ddfb78529f3660ab554f85f9fbe9d
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-03-01 13:14:49 +0530

    create code for selection and bubble sort

---
+++ src/arrayspackage/Arraylength.java
@@ -0,0 +1,19 @@
+package arrayspackage;
+
+
+public class Arraylength {
+    public static void main(String[] args) {
+       int a[] =new int[4];
+     System.out.println(a.length);
+ 
+    
+    
+   
+       
+    
+    
+      
+    
+    
+}
+}
--- src/hellopackage/HelloApp.java
+++ src/hellopackage/HelloApp.java
@@ -1,5 +1,6 @@
 package hellopackage;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HelloApp {
@@ -58,23 +59,65 @@ public class HelloApp {
         System.out.print(sqt);
 
     }
-    
-    static void FebonacciSerise(){
-        int n=9,fib=0,n1=0,n2=1,n3;
-         System.out.print(n1+","+n2);
-        for(int i=1;i<n;i++){
-            n3=n1+n2;
-             System.out.print(","+n3);
-            n1=n2;
-            n2=n3;
-           
+
+    static void FebonacciSerise() {
+        int n = 10, fib = 0, n1 = 0, n2 = 1, n3;
+        System.out.print(n1 + "," + n2);
+        for (int i = 1; i < n; i++) {
+            n3 = n1 + n2;
+            System.out.print("," + n3);
+            n1 = n2;
+            n2 = n3;
+
+        }
+    }
+
+    static void selectionSort() {
+        int a[] = {0, -20, 4, -3, 5, -1};
+        int i;
+        System.out.println(Arrays.toString(a));
+        for (int j = 0; j <= a.length - 1; j++) {
+            int minpos = j;
+            for (i = j + 1; i < a.length; i++) {
+
+                if (a[minpos] > a[i]) {
+                    minpos = i;
+                    System.out.println(Arrays.toString(a));
+
+                }
+
+            }
+            int t = a[j];
+            a[j] = a[minpos];
+            a[minpos] = t;
+        }
+        System.out.println("Swaped Aray " + Arrays.toString(a));
+
+    }
+
+    static void bubbleSort() {
+        int a[] = {12, 3, 8, 2, 0};
+        int i, j;
+        System.out.println(Arrays.toString(a));
+
+        for (i = 0; i < a.length - 1; i++) {
+            for (j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    System.out.println(Arrays.toString(a));
+
+                }
+            }
+
         }
     }
 
     public static void main(String[] args) {
 
-        FebonacciSerise();
-        System.out.println("");
+        bubbleSort();
+//        System.out.println("");
 
     }
 }
