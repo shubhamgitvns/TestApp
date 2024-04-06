174e6004c5dc22f0fa541b3f04d08815843541c3
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-04-06 12:41:05 +0530

    create some functions

--- src/hellopackage/HelloApp.java
+++ src/hellopackage/HelloApp.java
@@ -5,117 +5,7 @@ import java.util.Scanner;
 
 public class HelloApp {
 
-    static void Max_Factorof_Twoint() {
-        int num = 10;
-        int num_b = 15;
-        int temp = 0, max = 0;
-        //System.out.print("Deviser :");
-        for (int i = 2; i < num + num_b; i++) {
-            if (num % i == 0 || num_b % i == 0) {
-                temp = i;
-                //System.out.print(temp + ","); 
-                if (num % temp == 0 && num_b % temp == 0) {
-                    max = temp;
-                    System.out.println("");
-                    System.out.print("Max=" + max + ",");
-
-                }
-
-            }
-
-        }
-    }
-
-    static void PrimeNumber() {
-        int num = 4, count = 0, i;
-        if (num <= 1) {
-            System.out.print("This number is not prim");
-            return;
-        }
-        for (i = 1; i <= num / 2; i++) {
-            if (num % i == 0) {
-                count++;
-            }
-        }
-        if (count > 1) {
-            System.out.println(num + " The number is not prime");
-        } else {
-            System.out.println(num + " The number is prime");
-        }
-    }
-
-    static void LargePower() {
-        int x = 5,
-                n = 4,
-                sqt = 1;
-        if (x == 1) {
-            System.out.print(x);
-            return;
-        }
-        for (int i = 1; i <= n; i++) {
-            sqt = sqt * x;
-
-        }
-        System.out.print(sqt);
-
-    }
-
-    static void FebonacciSerise() {
-        int n = 10, fib = 0, n1 = 0, n2 = 1, n3;
-        System.out.print(n1 + "," + n2);
-        for (int i = 1; i < n; i++) {
-            n3 = n1 + n2;
-            System.out.print("," + n3);
-            n1 = n2;
-            n2 = n3;
-
-        }
-    }
-
-    static void selectionSort() {
-        int a[] = {0, -20, 4, -3, 5, -1};
-        int i;
-        System.out.println(Arrays.toString(a));
-        for (int j = 0; j <= a.length - 1; j++) {
-            int minpos = j;
-            for (i = j + 1; i < a.length; i++) {
-
-                if (a[minpos] > a[i]) {
-                    minpos = i;
-                    System.out.println(Arrays.toString(a));
-
-                }
-
-            }
-            int t = a[j];
-            a[j] = a[minpos];
-            a[minpos] = t;
-        }
-        System.out.println("Swaped Aray " + Arrays.toString(a));
-
-    }
-
-    static void bubbleSort() {
-        int a[] = {12, 3, 8, 2, 0};
-        int i, j;
-        System.out.println(Arrays.toString(a));
-
-        for (i = 0; i < a.length - 1; i++) {
-            for (j = 0; j < a.length - 1 - i; j++) {
-                if (a[j] > a[j + 1]) {
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
-                    System.out.println(Arrays.toString(a));
-
-                }
-            }
-
-        }
-    }
-
-    static void insertionSort(int [] a) {
-        
+    static void insertionSort(int[] a) {
 
         for (int i = 0; i <= a.length - 2; i++) {
             if (a[i] <= a[i + 1]) {
@@ -132,10 +22,56 @@ public class HelloApp {
 
     }
 
-    public static void main(String[] args) {
-int a[] = {0, 2, 3, 4, 5, 1, -1};
-        insertionSort(a);
-        System.out.println(Arrays.toString(a));
+    static int multiplication(int a, int b) {
+        int mult = a * b;
+        return mult;
 
     }
+
+    static void evenOdd(int a) {
+
+        if (a % 2 != 0) {
+            System.out.print("Odd ");
+        } else {
+            System.out.print("Even ");
+        }
+    }
+
+    static double average(int a, int b, int c) {
+        int sum = a + b + c;
+        double avg = sum / 3.0;
+        return avg;
+
+    }
+
+    static void factorial(int num) {
+        int fact = 1;
+        if (num == 0) {
+            System.out.print("Number is Zero");
+        }
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+
+        }
+        System.out.print(fact);
+
+    }
+
+    public static void main(String[] args) {
+        int a[]={1,2,3,4};
+        int i;
+        int temp;
+            temp=a[0];
+            a[0]=a[3];
+            a[3]=temp;
+        for(i=0;i<a.length;i++){
+            
+             System.out.print(a[i]+ ",");
+
+           
+        }
+       
+        
+    }
+
 }
